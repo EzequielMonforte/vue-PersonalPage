c22707e767b649918c820d5ee2ac20855340d42e
Ezequiel Monforte <ezequiel@debian.cedev.com>
2019-01-21 20:14:27 -0300

    partial-crrfix

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,9 +1,7 @@
 <template>
     <div>
         <h2>Hi there!</h2>
-        <p>I'm Ezequiel Monforte, a software developer living in Cordoba, Argentina.
-            Im passionated about technology and learning blalalsldlfsfdfldfdlsdl
-            lfdsflsdkflsdkfksldf <a href="">dd</a>
+        <p> <a href="">dd</a>
             dlfkdlfdslkvmvmz. dlfkdlffl
         </p>
     </div>
